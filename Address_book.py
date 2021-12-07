a293eca161527779b4b2986ae1788be54ce641ac
Arkadiusz Misiewicz <misiewicz.arkadiusz@gmail.com>
2021-12-07 13:47:39 +0100

    zmiana definicji __str__

--- Address_book.py
+++ Address_book.py
@@ -9,6 +9,9 @@ fake.job()
 fake.email()
 
 class AdrBook:
+   def __str__(self):
+        return f'{self.first_name} {self.surname} {self.email}'
+
    def __init__(self, first_name,surname,firm_name,status,email):
         self.first_name=first_name
         self.surname=surname
@@ -22,15 +25,11 @@ for _ in range(5):
     adr_list.append(AdrBook(first_name=fake.first_name(),surname=fake.last_name(),firm_name=fake.company(),status=fake.job(),email=fake.email()))
 
 for i in range(5):
-   print('%s %s, %s, %s, %s' % (adr_list[i].first_name, adr_list[i].surname, adr_list[i].firm_name, adr_list[i].status, adr_list[i].email))
-   #print(adr_list[i].first_name)
-   #print(adr_list[i].surname)
-   #print(adr_list[i].firm_name)
-   #print(adr_list[i].status)
-   #print(adr_list[i].email)
+   #print('%s %s, %s, %s, %s' % (adr_list[i].first_name, adr_list[i].surname, adr_list[i].firm_name, adr_list[i].status, adr_list[i].email))
+   print(adr_list[i])
+      
    #print('-----------')
 
-#print(wiz1.first_name)
 
 
 
